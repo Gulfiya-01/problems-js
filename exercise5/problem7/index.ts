f92101ae205b35ed81f68094c0532f92173617d7
Gulfiya-01 <userbaeva.gm@mail.ru>
2023-11-10 14:56:57 +0600

    added problem7

--- exercise5/problem7/index.ts
+++ exercise5/problem7/index.ts
@@ -1,5 +1,28 @@
-async function fetchPokemons() {
-  // Your code
+async function fetchPokemons(pokemonNames: string[]) {
+    try {
+        const pokemons = await Promise.all(
+            pokemonNames.map(async (name) => {
+                const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
+                if (!response.ok) {
+                    throw new Error("smth went wrong");
+                }
+                return response.json();
+            })
+        );
+
+        const result = pokemons.map((pokemon) => ({
+            id: pokemon.id,
+            name: pokemon.name,
+            height: pokemon.height,
+            weight: pokemon.weight,
+            image: pokemon.sprites.front_default,
+          }));
+      
+        
+        return result;
+    } catch (error) {
+        return Promise.reject(new Error("smth went wrong"));
+    }
 }
 
 export default fetchPokemons;
